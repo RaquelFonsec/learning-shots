ea9524ede18e0ec32d06ba8b2964b4cd055065ec
George Simei <georgesimei@gmail.com>
2023-03-09 19:30:24 +0100

    criacao de model-video_content

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_03_08_192125) do
+ActiveRecord::Schema[7.0].define(version: 2023_03_09_182757) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "trails", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.string "title"
+    t.text "description"
+    t.string "category"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_trails_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -26,4 +36,17 @@ ActiveRecord::Schema[7.0].define(version: 2023_03_08_192125) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  create_table "video_contents", force: :cascade do |t|
+    t.string "title"
+    t.string "thumb_url"
+    t.text "description"
+    t.string "video_id"
+    t.bigint "trail_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["trail_id"], name: "index_video_contents_on_trail_id"
+  end
+
+  add_foreign_key "trails", "users"
+  add_foreign_key "video_contents", "trails"
 end
