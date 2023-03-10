5b431e62b042259405ba1cc1fe68bca4829da46f
RaquelFonsec <raque-leto@hotmail.com>
2023-03-10 20:27:55 +0100

    search criado

--- app/javascript/controllers/youtube_search_controller.js
+++ app/javascript/controllers/youtube_search_controller.js
@@ -1,7 +1,7 @@
 import { Controller } from "@hotwired/stimulus"
 
-// Connects to data-controller="youtube-search"
+
 export default class extends Controller {
-  connect() {
-  }
+  static targets = [ "query", "results" ];
+
 }
