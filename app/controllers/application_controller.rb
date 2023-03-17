e705214e14b34cec2712b757950487c04c9f056d
Gilmar Ribeiro <gilmar_ribeiro@hotmail.com>
2023-03-17 11:39:20 -0300

    social Metatags

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,6 +10,10 @@ class ApplicationController < ActionController::Base
     devise_parameter_sanitizer.permit(:account_update, keys: [:first_name, :last_name, :photo])
   end
 
+  def default_url_options
+    { host: ENV["DOMAIN"] || "localhost:3000" }
+  end
+
 
 
 
